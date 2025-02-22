4724b77a7ebcafc6097006f48849e8efcfd6f2e3
Robrecht Cannoodt <rcannood@gmail.com>
2025-02-22 22:04:46 +0100

    fix mermaid diagrams, remove html comments from text

--- _extensions/rcannood/openproblemsdocs/openproblemsdocs.lua
+++ _extensions/rcannood/openproblemsdocs/openproblemsdocs.lua
@@ -1,9 +1,22 @@
 
 return {
   {
-    Callout = function(callout)
-      local calloutContent = pandoc.utils.stringify(callout.content)
-      return pandoc.RawBlock("html", "<Callout variant=\"" .. callout.type .. "\">" .. calloutContent .. "</Callout>")
-    end
+    -- wrap all divs with class="callout" in a Callout element
+    Callout = function(el)
+      local calloutContent = pandoc.utils.stringify(el.content)
+      return pandoc.RawBlock("html", "<Callout variant=\"" .. el.type .. "\">\n" .. calloutContent .. "\n</Callout>")
+    end,
+    -- wrap all codeblocks with class="mermaid" in a Mermaid element
+    CodeBlock = function(el)
+      if el.attr.classes:includes("mermaid") then
+        local escapedTest = el.text:gsub("`", "\\`"):gsub("&#10;", "\n"):gsub("&#10;", "\n")
+        return pandoc.RawBlock("html", "<MermaidDiagram chart={`\n" .. escapedTest .. "`} />")
+      end
+    end,
+    -- remove simple html comments from text
+    RawBlock = function(el)
+      el.text = el.text:gsub("<!--.*-->", "")
+      return el
+    end,
   }
 }
