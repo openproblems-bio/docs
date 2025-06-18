413f0318ad4a5baf3ae7cdeadc8a34f70b2da60b
Robrecht Cannoodt <rcannood@gmail.com>
2025-06-18 14:14:17 +0200

    fix plugin

--- _extensions/rcannood/openproblemsdocs/openproblemsdocs.lua
+++ _extensions/rcannood/openproblemsdocs/openproblemsdocs.lua
@@ -4,19 +4,19 @@ return {
     -- wrap all divs with class="callout" in a Callout element
     Callout = function(el)
       local calloutContent = pandoc.utils.stringify(el.content)
-      return pandoc.RawBlock("html", "<Callout variant=\"" .. el.type .. "\">\n" .. calloutContent .. "\n</Callout>")
+      return pandoc.RawBlock("markdown", "<Callout variant=\"" .. el.type .. "\">\n" .. calloutContent .. "\n</Callout>")
     end,
     -- wrap all codeblocks with class="mermaid" in a Mermaid element
     CodeBlock = function(el)
       if el.attr.classes:includes("mermaid") then
         local escapedTest = el.text:gsub("`", "\\`")
-        return pandoc.RawBlock("html", "<MermaidDiagram chart={`\n" .. escapedTest .. "`} />")
+        return pandoc.RawBlock("markdown", "<MermaidDiagram chart={`\n" .. escapedTest .. "`} />")
       end
     end,
     -- remove simple html comments from text
     RawBlock = function(el)
       el.text = el.text:gsub("<!--.*-->", "")
       return el
-    end,
+    end
   }
 }
