c6099a9fa85d1a00bcf3298503d22ad5cb79a03e
Robrecht Cannoodt <rcannood@gmail.com>
2025-06-18 14:15:28 +0200

    trim doc tree

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
 QUARTO_CHROMIUM_HEADLESS_MODE=new \
-  /home/rcannood/.local/share/quarto-1.6.40/bin/quarto render \
+  quarto render \
   --profile evaluate_code
 
 rm -r ../website-new/src/routes/documentation
 cp -r _docs ../website-new/src/routes/documentation
+
+
+# note: see https://github.com/quarto-dev/quarto-cli/tree/v1.6.40/src/resources/filters/customnodes
